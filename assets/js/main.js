30fd7a83a175bb750f43d269e5d58354c09092b4
Csaba Okrona <ochronus@ochronus.com>
2020-09-19 07:20:58 +0200

    on-demand disqus loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 // Some code could be here ...
+
+const leaveCommentsButton = document.querySelector("#loadComments");
+
+leaveCommentsButton &&
+leaveCommentsButton.addEventListener(
+    "click",
+    (e) => {
+        e.preventDefault();
+        var disqus_config = function () {};
+
+        (function() { 
+        var d = document, s = d.createElement('script');
+        s.src = 'https://ochronus-online.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+        leaveCommentsButton.remove();
+    }
+  );
